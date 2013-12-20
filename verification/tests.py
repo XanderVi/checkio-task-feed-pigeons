5e7471a6817ba326ece8854a1b2bf0eed66a2720
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-12-20 15:29:40 +1200

    referee and tests

--- verification/tests.py
+++ verification/tests.py
@@ -15,15 +15,20 @@ TESTS = {
         {'answer': 6, 'input': 10},
     ],
     "Extra": [
-        {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
-        },
-        {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
+        {'answer': 45, 'input': 187},
+        {'answer': 45, 'input': 171},
+        {'answer': 36, 'input': 149},
+        {'answer': 34, 'input': 118},
+        {'answer': 28, 'input': 95},
+        {'answer': 36, 'input': 142},
+        {'answer': 15, 'input': 41},
+        {'answer': 15, 'input': 48},
+        {'answer': 15, 'input': 42},
+        {'answer': 45, 'input': 190},
+        {'answer': 19, 'input': 54},
+        {'answer': 21, 'input': 68},
+        {'answer': 16, 'input': 51},
+        {'answer': 28, 'input': 89},
+        {'answer': 7, 'input': 17},
     ]
 }

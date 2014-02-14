c05b80ebf59db851820c164c010b6bdee4015778
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-02-14 16:24:14 +1200

    add tests

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -77,8 +77,10 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
             }
             //Dont change the code before it
 
-            var canvas = new PigeonsCanvas();
-            canvas.create($content.find(".explanation")[0], explanation);
+            if (explanation) {
+                var canvas = new PigeonsCanvas();
+                canvas.create($content.find(".explanation")[0], explanation);
+            }
 
             this_e.setAnimationHeight($content.height() + 60);
 
@@ -119,7 +121,7 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
             var colorGrey1 = "#EBEDED";
 
             var colorWhite = "#FFFFFF";
-            
+
             var bird = "M14.676,22.594 C14.676,22.594 3.305,21.071 0,10.526 C0,10.526 7.114,13.571 15.517,11.485 C15.517,11.485 16.637,9.906 12.212,5 C12.212,5 25.488,6.128 26.048,13.177 C26.048,13.177 31.763,5.369 38.651,10.639 C40.052,11.711 40.444,13.346 39.436,12.556 C39.436,12.556 34.45,12.556 32.826,18.647 C32.826,18.647 32.266,31.222 15.965,29.53 C15.965,29.53 11.763,33.139 11.763,35 L4.369,32.124 C4.369,32.124 15.293,26.09 14.676,22.594 z";
 
             var padding = 10;
@@ -132,7 +134,7 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
 
             var paper;
 
-            this.create = function(dom, feeding) {
+            this.create = function (dom, feeding) {
                 paper = Raphael(dom,
                     Math.min(feeding.length, row) * (cell + padding) - padding,
                     Math.ceil(feeding.length / row) * cell);
